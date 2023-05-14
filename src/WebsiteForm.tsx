6ca46db374225e0d97067547625ab38711efbbdf
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-05-14 20:37:25 +0300

    awe

--- src/WebsiteForm.tsx
+++ src/WebsiteForm.tsx
@@ -5,12 +5,15 @@ import Form from "./Form.js";
 import Result from "./Result.js";
 import { useState } from "react";
 
+import CaphubThemeProvider from "@caphub-group/theme-provider";
+
 const WebsiteForm = () => {
   const [loanAmount, setLoanAmount] = useState(0);
   const [interest, setInterest] = useState(0);
   const [amortization, setAmortization] = useState(0);
 
   return (
+    <CaphubThemeProvider>
     <Grid container direction={{ xs: "column", md: "row" }} spacing={2}>
       <Grid item xs={12} md={6}>
         <Form
@@ -28,6 +31,7 @@ const WebsiteForm = () => {
         />
       </Grid>
     </Grid>
+    </CaphubThemeProvider>
   );
 };
 export default WebsiteForm;
